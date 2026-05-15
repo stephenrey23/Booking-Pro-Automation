9452bf4613f479eba2993496fb81dbaffbe07899
stephenrey23 <stephenjreyes41@gmail.com>
2026-05-15 13:20:49 -0600

    selectors.js

--- cypress/support/selectors.js
+++ cypress/support/selectors.js
@@ -1,12 +1,20 @@
 export const EL = {
+    // Búsqueda
     destInput: 'input[name="ss"]', 
     searchBtn: 'button[type="submit"]',
     
+    hotelCard: '[data-testid="property-card"]',
+    availabilityBtn: '[data-testid="availability-cta-button"]',
+    roomSelect: 'select[data-testid="hprt-nos-select"]',
+    reserveBtn: 'button.txp-bui-main-action',
+
     fName: '#firstname',
     lName: '#lastname',
     email: '#email',
     address: '#address',
     city: '#city',
     phone: 'input[type="tel"]',
+    
+    submitDetails: 'button[name="book"]',
     completeBooking: 'button:contains("Complete booking")'
 };
